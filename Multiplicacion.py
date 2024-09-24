67c7b2769cf2a0d1bb023e6ba7ded5ca8456d8bd
TorresOlveraGael <gaeltorreso23.cb128@dgeti.sems.gob.mx>
2024-09-23 20:33:06 -0600

    Add files via upload

---
+++ Multiplicacion.py
@@ -0,0 +1,15 @@
+#En esta actividad realizaremos un codigo que solicita 3 numeros y los multplica
+
+print (" ") 
+print ("Torres Olvera Gael") 
+print (" ")
+
+num1 = int(input("Escribe un numero: ")) #Nos pide introducir un valor
+
+num2 = int(input("Escribe un numero: ")) #Nos pide introducir un valor
+
+num3 = int(input("Escribe un numero: ")) #Nos pide introducir un valor
+
+mult = num1*num2*num3 #Indica al sistema la operacion a realizar
+
+print ("El resultado de la multiplicacion es: ", mult) #El resultado final
